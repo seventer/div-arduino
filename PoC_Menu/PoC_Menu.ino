41820ba9e7ec64301d94795d88b7e84a7467a869
Gerard van Seventer <seventer@users.noreply.github.com>
2017-03-04 23:48:50 +0100

    initial upload of poc files
    proof of concept for adding mini menu to store/retrieve some values from arduino eeprom

---
+++ PoC_Menu/PoC_Menu.ino
@@ -0,0 +1,49 @@
+/*
+ * Proof of concept 
+ * Add mini menu changing settings 
+ * and store them into EEPROM
+ */
+
+#include "setup.h"
+
+const int ledPin =  LED_BUILTIN;
+const long interval = 1000;
+
+
+// Max lenght of array is define in setup.h 
+// use somewhere between 32..63 depending on memory need
+char SSID_ESP[LINE_LENGTH];
+char SSID_KEY[LINE_LENGTH];
+
+int ledState = LOW;
+unsigned long previousMillis = 0;
+
+Setup stp;
+
+void setup() {
+  Serial.begin(9600);
+  stp.begin(SSID_ESP,SSID_KEY);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (stp.getSerialChar()=='M') {
+    stp.menu();
+  }
+  
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState);
+  }
+  
+}
+
+
+
