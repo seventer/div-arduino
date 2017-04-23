d0084a42dc591f4be00d090f9af25f0165394602
Gerard van Seventer <seventer@users.noreply.github.com>
2017-04-23 22:29:43 +0200

    initial upload of LCD test code
    Some LCD testing with a LM335 temperature sensor and an interrupt routine to monitor a reed switch

---
+++ lcd_test1/lcd_test1.ino
@@ -0,0 +1,55 @@
+/*
+ Proof of concept LCD + some I/O + interrupt
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
+const float VOLTAGE = 4.8;
+const int LM337_PIN = 6;
+
+int val = 0; 
+float temp=0;
+
+int intCounter=0;
+
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.print("Starting.....");
+  Serial.begin(9600);  
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), intWorker, CHANGE);
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+  lcd.clear();  
+  val = analogRead(LM337_PIN);
+  lcd.print("RAW: ");
+  lcd.print(val);
+  lcd.setCursor(0, 1);
+//  temp = (VOLTAGE * val) / 1024.0 * 100.0 - 273.15;
+//  lcd.print("Temp: ");
+//  lcd.print(temp);
+  lcd.print("intCnt: ");
+  lcd.print(intCounter);
+  Serial.println(val);
+  delay(1000); 
+}
+
+void intWorker() {
+  intCounter++;
+  state = !state;
+}
+
